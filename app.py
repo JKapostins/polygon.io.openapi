c15e78782bf3e721ebc3a392d9307221e24b374e
Jake Kapostins <jkapostins@gnarlysoft.net>
2023-12-27 01:14:51 -0500

    aider: Refactored app to parse and convert navigation elements to markdown format.
    # Aider chat conversation:
    
    ASSISTANT: ```diff
    --- app.py
    +++ app.py
    @@ ... @@
    +import requests
    +from bs4 import BeautifulSoup
    +
    +def parse_html(url):
    +    response = requests.get(url)
    +    return BeautifulSoup(response.text, 'html.parser')
    +
    +def convert_nav_to_markdown(nav_element):
    +    markdown = ""
    +    for item in nav_element.find_all(['a', 'ul', 'li']):
    +        if item.name == 'a':
    +            markdown += f"- [{item.text.strip()}]({item['href']})\n"
    +        elif item.name == 'ul':
    +            markdown += ""
    +        elif item.name == 'li':
    +            markdown += ""  # No additional indentation for hierarchy
    +    return markdown
    +
    +def main():
    +    url = "https://polygon.io/docs/stocks/getting-started"
    +    soup = parse_html(url)
    +    nav_elements = soup.find_all('nav')
    +    if len(nav_elements) > 1:
    +        nav_markdown = convert_nav_to_markdown(nav_elements[1])
    +        print(nav_markdown)  # For demonstration purposes, print the markdown
    +
    +if __name__ == "__main__":
    +    main()
    ```

--- app.py
+++ app.py
@@ -10,3 +10,31 @@
 #     Convert this to a list of endpoints and their hierarchy to a markdown format.
 
 
+import requests
+from bs4 import BeautifulSoup
+
+def parse_html(url):
+    response = requests.get(url)
+    return BeautifulSoup(response.text, 'html.parser')
+
+def convert_nav_to_markdown(nav_element):
+    markdown = ""
+    for item in nav_element.find_all(['a', 'ul', 'li']):
+        if item.name == 'a':
+            markdown += f"- [{item.text.strip()}]({item['href']})\n"
+        elif item.name == 'ul':
+            markdown += ""
+        elif item.name == 'li':
+            markdown += ""  # No additional indentation for hierarchy
+    return markdown
+
+def main():
+    url = "https://polygon.io/docs/stocks/getting-started"
+    soup = parse_html(url)
+    nav_elements = soup.find_all('nav')
+    if len(nav_elements) > 1:
+        nav_markdown = convert_nav_to_markdown(nav_elements[1])
+        print(nav_markdown)  # For demonstration purposes, print the markdown
+
+if __name__ == "__main__":
+    main()
