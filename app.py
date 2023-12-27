b6c15d8dccab3dd98ae051df18c98fa36d277bfd
Jake Kapostins <jkapostins@gnarlysoft.net>
2023-12-27 00:30:31 -0500

    Refactored app to parse Polygon.io API documentation and extract relevant information for Open API Spec generation.

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("Hello, World!")
+# This application is responsible for converting the Polygon.io API documentation to an OPEN API 3.0 specification.
+# The source of the documentation is on the polygon.io website in html form.
+# The application will parse the html and minimize content to only the relevant information required to generate the Open API Spec.
+
+# 1.) Parse the html document found at https://polygon.io/docs/stocks/getting-started and extract the anchors out of the first div. These anchors will be our urls for differnet documentations (stocks, forex, crypto, etc.)
+
+# 2.) Print the anchors so we can see what we are working with.
