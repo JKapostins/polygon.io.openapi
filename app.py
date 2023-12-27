2ce60846099e2d994acd704ae07f9d21e0df7a02
Jake Kapostins <jkapostins@gnarlysoft.net>
2023-12-27 00:59:12 -0500

    aider: Refactored app to parse HTML, convert navigation to markdown, and save to file.
    # Aider chat conversation:
    
    ASSISTANT: ```diff
    --- app.py
    +++ app.py
    @@ ... @@
    +import requests
    +from bs4 import BeautifulSoup
    +
    +def parse_html(url):
    +    response = requests.get(url)
    +    return BeautifulSoup(response.text, 'html.parser')
    +
    +def convert_nav_to_markdown(nav_element):
    +    markdown = ""
    +    for item in nav_element.find_all(['a', 'ul', 'li']):
    +        if item.name == 'a':
    +            markdown += f"### [{item.text.strip()}]({item['href']})\n"
    +        elif item.name == 'ul':
    +            markdown += "\n"
    +        elif item.name == 'li':
    +            markdown += "    "  # Indentation for hierarchy
    +    return markdown
    +
    +def save_markdown_to_file(markdown, filename):
    +    with open(filename, 'w') as file:
    +        file.write(markdown)
    +
    +def main():
    +    url = "https://polygon.io/docs/stocks/getting-started"
    +    soup = parse_html(url)
    +    nav_elements = soup.find_all('nav')
    +    if len(nav_elements) > 1:
    +        nav_markdown = convert_nav_to_markdown(nav_elements[1])
    +        save_markdown_to_file(nav_markdown, "navigation.md")
    +
    +if __name__ == "__main__":
    +    main()
    ```

--- app.py
+++ app.py
@@ -11,3 +11,35 @@
 #     Save the markdown to a file called "navigation.md"
 
 
+import requests
+from bs4 import BeautifulSoup
+
+def parse_html(url):
+    response = requests.get(url)
+    return BeautifulSoup(response.text, 'html.parser')
+
+def convert_nav_to_markdown(nav_element):
+    markdown = ""
+    for item in nav_element.find_all(['a', 'ul', 'li']):
+        if item.name == 'a':
+            markdown += f"### [{item.text.strip()}]({item['href']})\n"
+        elif item.name == 'ul':
+            markdown += "\n"
+        elif item.name == 'li':
+            markdown += "    "  # Indentation for hierarchy
+    return markdown
+
+def save_markdown_to_file(markdown, filename):
+    with open(filename, 'w') as file:
+        file.write(markdown)
+
+def main():
+    url = "https://polygon.io/docs/stocks/getting-started"
+    soup = parse_html(url)
+    nav_elements = soup.find_all('nav')
+    if len(nav_elements) > 1:
+        nav_markdown = convert_nav_to_markdown(nav_elements[1])
+        save_markdown_to_file(nav_markdown, "navigation.md")
+
+if __name__ == "__main__":
+    main()
